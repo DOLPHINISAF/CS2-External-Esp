c4ae508c3431944b8c8b3300916867552bafe15a
Catalin Nedelescu <catalin.nedelescu@yahoo.com>
2024-04-28 00:35:45 +0300

    updated offsets

--- src/offsets.h
+++ src/offsets.h
@@ -1,8 +1,8 @@
 namespace offsets {
 	constexpr std::ptrdiff_t dwEntityList = 0x18C6268;
-	constexpr std::ptrdiff_t dwLocalPlayerController = 0x1915C18;
-	constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x173A3C8;
-	constexpr std::ptrdiff_t dwViewMatrix = 0x19278B0;
+	constexpr std::ptrdiff_t dwLocalPlayerController = 0x1915C08;
+	constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x173A3B8;
+	constexpr std::ptrdiff_t dwViewMatrix = 0x19278A0;
 	constexpr std::ptrdiff_t m_iHealth = 0x334;
 	constexpr std::ptrdiff_t m_lifeState = 0x338;
 	constexpr std::ptrdiff_t m_vOldOrigin = 0x127C;
